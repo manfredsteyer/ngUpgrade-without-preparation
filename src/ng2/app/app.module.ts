eab710a55236e30daf73b2cbb0698ec7a3e43180
ManfredSteyer <manfred.steyer@gmx.net>
2017-07-13 21:21:44 +0200

    sibling outlets

--- src/ng2/app/app.module.ts
+++ src/ng2/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, InjectionToken } from '@angular/core';
+import { RouterModule, UrlHandlingStrategy } from '@angular/router';
+
 import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 import { AppComponent } from './app.component';
 import { Ng2DemoComponent } from "ng2/app/ng2-demo.component";
 import { phoneServiceProvider } from "ng2/app/phone.service";
 
 declare var angular: any;
+
+class CustomHandlingStrategy implements UrlHandlingStrategy {
+  shouldProcessUrl(url) {
+    return url.toString().startsWith("/feature1") || url.toString() === "/";
+  }
+  extract(url) { return url; }
+  merge(url, whole) { return url; }
+}
 
 angular.module('phonecatApp')
   .directive(
@@ -20,13 +30,24 @@ angular.module('phonecatApp')
   ],
   imports: [
     BrowserModule,
-    UpgradeModule
+    UpgradeModule,
+    RouterModule.forRoot([
+      {
+        path: 'ng2-route',
+        component: Ng2DemoComponent
+      }
+    ],
+    {
+      useHash: true
+    }
+    )
   ],
   entryComponents: [
     Ng2DemoComponent // Don't forget this!!!
   ],
   providers: [
-    phoneServiceProvider
+    phoneServiceProvider,
+    { provide: UrlHandlingStrategy, useClass: CustomHandlingStrategy }
   ],
   bootstrap: [AppComponent]
 })
