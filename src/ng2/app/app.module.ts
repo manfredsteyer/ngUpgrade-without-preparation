a9b8fc96a1fdda492fa294198e42a57dba71cb1c
ManfredSteyer <manfred.steyer@gmx.net>
2017-07-13 20:46:21 +0200

    bootstrap hybrid app

--- src/ng2/app/app.module.ts
+++ src/ng2/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 import { AppComponent } from './app.component';
+import { Ng2DemoComponent } from "ng2/app/ng2-demo.component";
+
+declare var angular: any;
+
+angular.module('phonecatApp')
+  .directive(
+    'ng2Demo',
+    downgradeComponent({component: Ng2DemoComponent})
+  );
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ng2DemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
+  ],
+  entryComponents: [
+    Ng2DemoComponent // Don't forget this!!!
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent] // No Bootstrap-Component
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['phonecatApp'], { strictDi: true });
+  }
+}
